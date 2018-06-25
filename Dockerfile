31b356c066d8f5c863b951a18767dd3e012ec926
Matthias Wiesner <matthias.wiesner@googlemail.com>
2018-06-25 16:25:15 +0200

    First working commit with docker

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:10.5.0-stretch
+
+RUN apt-get update && apt-get install -y \
+    ghostscript \
+    unoconv \
+    ffmpeg \
+    imagemagick \
+    curl
+
+COPY . /home/node/app
+
+RUN cd /home/node/app && npm install 
+
+WORKDIR /home/node/app
+EXPOSE 3000
+
+CMD node app.js
--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const fs                = require('fs');
-const path              = require('path');
 
 const bodyParser        = require('body-parser');
 const config            = require('config');
@@ -9,27 +8,39 @@ const kue               = require('kue');
 const passport          = require('passport');
 const Strategy          = require('passport-http').BasicStrategy;
 const request           = require('request');
-const syncrequest       = require('sync-request');
 const tmp               = require('tmp');
 const util              = require('util');
 const winston           = require('winston');
 const DailyRotateFile   = require('winston-daily-rotate-file');
 const xml2js            = require('xml2js');
 
-const users             = require('./users');
-const mimetypes         = require('./mimetype.json');
+
+// get mimetype definitions from filepreview-es6 package
+const mimetypes         = require('./node_modules/filepreview-es6/db.json');
 
 
+// config definitions
+const appConfig         = config.get('app');
+const authUsers         = [appConfig.authUser];
+const jobqueueConfig    = config.get('jobqueue');
 
-const app = express();
-app.use(bodyParser.json())
 
-let appConfig = config.get('app');
-
-
+// helper function for authentification
+function findByUsername(username, callback) {
+    process.nextTick(function() {
+        for (var i = 0, len = authUsers.length; i < len; i++) {
+            var record = authUsers[i];
+            if (record.username === username) {
+                return callback(null, record);
+            }
+        }
+        return callback(null, null);
+    });
+};
+// define BasicAuth as authentification strategy
 passport.use(new Strategy(
     function(username, password, callback) {
-        users.findByUsername(username, function(error, user) {
+        findByUsername(username, function(error, user) {
             if (error) { return callback(error); }
             if (!user) { return callback(null, false); }
             if (user.password != password) { return callback(null, false); }
@@ -39,7 +50,8 @@ passport.use(new Strategy(
 ));
 
 
-const logLevel = (process.env.NODE_ENV == 'production' ? "error" : "info");
+// define logging handler, with file rotation
+const logLevel = (process.env.NODE_ENV == 'production' ? "error" : "debug");
 const logger = winston.createLogger({
     level: logLevel,
     transports: [
@@ -55,18 +67,26 @@ const logger = winston.createLogger({
 });
 
 
-const filePreviewQueue = kue.createQueue(config.get('jobqueue'));
+// define jobQueue handler
+const filePreviewQueue = kue.createQueue(jobqueueConfig);
 filePreviewQueue.process('filePreview', function (job, done) {
-    logger.info('processing job: ' + job.id);
+    logger.debug('processing job: ' + job.id);
     handleFilePreview(
         job.data.options,
         job.data.download_url,
-        job.data.ext,
         job.data.signed_s3_url,
         done
     );
 });
 
+
+// let's create the app
+const app = express();
+app.use(bodyParser.json())
+
+app.listen(appConfig.port || 3000, function() {
+    logger.info(util.format('Example app listening on port %s!', (appConfig.port || '3000')));
+});
 
 app.post('/filepreview', passport.authenticate('basic', { session: false }), function (req, res) {
     if (!req.body.callback_url) return res.status(422).send({error: 'request must contain callback_url'})
@@ -76,19 +96,6 @@ app.post('/filepreview', passport.authenticate('basic', { session: false }), fun
     const callback_url  = req.body.callback_url;
     const download_url  = req.body.download_url;
     const signed_s3_url = req.body.signed_s3_url;
-
-    // by by Content-Type
-    let ext;
-    const downloadfile_res = syncrequest('HEAD', download_url);
-    if (downloadfile_res.statusCode != 200){
-        return downloadfile_res.status(422).send({error: 'could not get download_url meta data'});
-    } else {
-        if (!mimetypes[downloadfile_res.headers['content-type']].extensions[0]){
-            return downloadfile_res.status(422).send({error: 'not supported content-type'});
-        }
-        ext = mimetypes[downloadfile_res.headers['content-type']].extensions[0];
-        logger.info(ext);
-    }
 
     let options = appConfig.options;
     if (req.body.options) {
@@ -100,29 +107,37 @@ app.post('/filepreview', passport.authenticate('basic', { session: false }), fun
         options.keepAspect = false;
     }
 
-    logger.info('---- Start new file preview request ----');
-    logger.info(options);
+    createJob(options, download_url, signed_s3_url, callback_url);
+    res.json("OK");
+});
+
+
+function createJob(options, download_url, signed_s3_url, callback_url){
+    logger.info('---- Start new file preview job ----');
+    logger.debug({
+        options: options,
+        download_url: download_url,
+        signed_s3_url, signed_s3_url
+    });
 
     const job = filePreviewQueue.create('filePreview', {
         options: options,
         download_url: download_url,
-        ext: ext,
         signed_s3_url, signed_s3_url
     });
 
     job.attempts(2).backoff(true).save();
-    
-    job.on('failed', function (err) {
-        return responseError(err, callback_url);
+
+    job.on('failed', function (error) {
+        logger.info(util.format('failed job, result: %s', error));
+        return responseError(error, callback_url);
     });
-    
+
     job.on('complete', function (result) {
         logger.info(util.format('completed job, result: %s', result));
         return responseSuccess(result, callback_url);
     });
-
-    res.json("OK");
-});
+}
 
 
 function responseError(error, callback_url){
@@ -133,7 +148,7 @@ function responseError(error, callback_url){
         body:    JSON.stringify({
             error: util.format('An error occured: %s', error)
         })
-    
+
     }, function(error){
         if (error){
             logger.error(error);
@@ -154,70 +169,51 @@ function responseSuccess(previewUrl, callback_url){
         if (error) {
             return logger.error(error);
         }
-        logger.info(previewUrl);
+        logger.debug(previewUrl);
     });
 }
 
 
-function handleFilePreview(options, download_url, ext, signed_s3_url, done){
-    logger.info('handleFilePreview called');
+function handleFilePreview(options, download_url, signed_s3_url, done){
+    logger.debug('handleFilePreview called');
 
-    const uploadFile = function(previewFileObj){
-        logger.info('uploadFile called');
-        fs.readFile(previewFileObj.name, function (error, data) {
-            if (error) {
-                logger.error(error);
-                done(new Error(error));
-            }
-            logger.info('upload file to AWS');
-            request.put({
-                headers: {'content-type' : util.format('image/%s', options.outputFormat)},
-                body: data,
-                url: signed_s3_url
+    checkDownload(download_url, done, function(ext){
+        downloadFile(options, download_url, ext, done, function(previewFileObj){
+            uploadFile(previewFileObj, signed_s3_url, options, done);
+        })
+    });
+};
 
-            }, function(error, response, body){
-                logger.info('response.statusCode: ' + response.statusCode);
 
-                previewFileObj.removeCallback();
-                if (error) {
-                    logger.error(error);
-                    done(new Error(error));
-                } else {
-                    if (response.statusCode > 200) {
-                        new xml2js.Parser().parseString(body, function (error, result) {
-                            if (error) {
-                                logger.error(error);
-                                // AWS Error could not be parsed
-                                done(new Error(error));
-                            } else {
-                                const error = util.format('%s (%s)', result.Error.Message, result.Error.Code);
-                                logger.error(error);
-                                done(new Error(error));
-                            }
-                        });
-                    } else {
-                        done(null, signed_s3_url.split('?')[0]);
-                    }
-                }
-            });
-        });
-    }
-    
+function checkDownload(download_url, done, next){
+    // get extension by by Content-Type
+    request.head(download_url, {timeout: 10000}, function (error, response) {
+        if (error || response.statusCode > 200) {
+            return done(new Error('could not get download_url meta data'));
+        }
+        if (!mimetypes[response.headers['content-type']].extensions[0]){
+            return done(new Error('not supported content-type'));
+        }
+        logger.debug(response.headers);
+        const ext = mimetypes[response.headers['content-type']].extensions[0];
+        next(ext);
+    });
+}
+
+
+function downloadFile(options, download_url, ext, done, next){
     const downloadFileObj = tmp.fileSync({postfix: '.' + ext});
-    logger.info('create temp download file: ' + downloadFileObj.name);
+    logger.debug('create temp download file: ' + downloadFileObj.name);
 
     const file = fs.createWriteStream(downloadFileObj.name);
-
     file.on('finish', function(){
         const previewFileObj = tmp.fileSync({postfix: '.' + options.outputFormat});
-        logger.info('create temp preview file: ' + previewFileObj.name);
-    
+        logger.debug('create temp preview file: ' + previewFileObj.name);
+
         filepreview.generateAsync(downloadFileObj.name, previewFileObj.name, options)
             .then(function(){
                 downloadFileObj.removeCallback();
-                if (signed_s3_url) {
-                    return uploadFile(previewFileObj);
-                }
+                next(previewFileObj);
             }).catch(function(error) {
                 logger.error(error);
                 done(new Error(error));
@@ -235,6 +231,43 @@ function handleFilePreview(options, download_url, ext, signed_s3_url, done){
 }
 
 
-app.listen(appConfig.port || 3000, function () {
-    logger.info(util.format('Example app listening on port %s!', (appConfig.port || '3000')));
-});
+function uploadFile(previewFileObj, signed_s3_url, options, done){
+    logger.debug('uploadFile called');
+    fs.readFile(previewFileObj.name, function (error, data) {
+        if (error) {
+            logger.error(error);
+            done(new Error(error));
+        }
+        logger.debug('upload file to AWS');
+        request.put({
+            headers: {'content-type' : util.format('image/%s', options.outputFormat)},
+            body: data,
+            url: signed_s3_url
+
+        }, function(error, response, body){
+            logger.debug('response.statusCode: ' + response.statusCode);
+
+            previewFileObj.removeCallback();
+            if (error) {
+                logger.error(error);
+                done(new Error(error));
+            } else {
+                if (response.statusCode > 200) {
+                    new xml2js.Parser().parseString(body, function (error, result) {
+                        if (error) {
+                            logger.error(error);
+                            // AWS Error could not be parsed
+                            done(new Error(error));
+                        } else {
+                            const error = util.format('%s (%s)', result.Error.Message, result.Error.Code);
+                            logger.error(error);
+                            done(new Error(error));
+                        }
+                    });
+                } else {
+                    done(null, signed_s3_url.split('?')[0]);
+                }
+            }
+        });
+    });
+};
